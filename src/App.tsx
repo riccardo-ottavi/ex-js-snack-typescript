d72690ba44c993b535146c5b4099a8977b1f9e0b
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-02 09:26:11 +0100

    inserisci snack 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 
 function App() {
  
+  let data : unknown
+
+  if(typeof data === "string"){
+    console.log(data.toUpperCase())
+  }else if(typeof data === "number"){
+    const res : number = data * 2;
+    console.log(res)
+  }else if(typeof data === "boolean"){
+    if (data === true){
+      console.log("Si")
+    }else if(data === false){
+      console.log("No")
+    }
+  }else{
+    console.log("Tipo non supportato")
+  }
+
   return (
     <>
       
